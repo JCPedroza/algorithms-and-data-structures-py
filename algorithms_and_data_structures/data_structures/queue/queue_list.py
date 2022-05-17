29ca7351800efdda7452295dcdf16f59c88265d4
JCPedroza <nemesis4go10@hotmail.com>
2022-05-17 03:05:50 -0500

    feat (stru): test, implement queue #8

---
+++ algorithms_and_data_structures/data_structures/queue/queue_list.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+
+class Queue:
+    """Implements a queue using native lists."""
+    def __init__(self):
+        self.list = []
+
+    def __str__(self) -> str:
+        return str(self.list)
+
+    def __len__(self) -> int:
+        return len(self.list)
+
+    def enqueue(self, item: Any) -> None:
+        self.list.append(item)
+
+    def dequeue(self) -> Any:
+        return self.list.pop(0)
+
+    def peek(self) -> Any:
+        return self.list[0]
+
+    def is_empty(self) -> bool:
+        return len(self) == 0
+
+    def clear(self) -> None:
+        self.list = []
+
+
+structure = Queue
+name = 'queue native list'
---
+++ algorithms_and_data_structures/data_structures/queue/test_queue.py
@@ -0,0 +1,35 @@
+from attr import validate
+from . import queue_list
+
+test_subjects = [
+    queue_list
+]
+
+
+def validate_state(queue, exp_len, exp_is_empty, exp_peek=''):
+    assert len(queue) == exp_len
+    assert queue.is_empty() == exp_is_empty
+
+    if exp_peek and not queue.is_empty():
+        assert queue.peek() == exp_peek
+
+
+def test_queue():
+    for subject in test_subjects:
+        queue = subject.structure()
+        validate_state(queue, 0, True)
+
+        queue.enqueue(2)
+        validate_state(queue, 1, False, 2)
+
+        queue.enqueue(4)
+        validate_state(queue, 2, False, 2)
+
+        queue.enqueue(6)
+        validate_state(queue, 3, False, 2)
+
+        assert queue.dequeue() == 2
+        validate_state(queue, 2, False, 4)
+
+        queue.clear()
+        validate_state(queue, 0, True)
