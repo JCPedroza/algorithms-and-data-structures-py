4e815e0b971806e11b181db9313add654854e6a1
JCPedroza <nemesis4go10@hotmail.com>
2022-05-20 02:10:57 -0500

    vari (eule): implement optimization for euler 1 # 1

--- algorithms_and_data_structures/project_euler/e001_multiples_of_3_or_5/test_mul_of_3_or_5.py
+++ algorithms_and_data_structures/project_euler/e001_multiples_of_3_or_5/test_mul_of_3_or_5.py
@@ -1,10 +1,18 @@
-from . import mul_of_3_or_5_naive
+from . import mul_of_3_or_5_filter
+from . import mul_of_3_or_5_analytic
+
 
 test_subjects = [
-    mul_of_3_or_5_naive
+    mul_of_3_or_5_filter,
+    mul_of_3_or_5_analytic
 ]
 
 
 def test_mul_of3or5():
     for subject in test_subjects:
+        assert subject.solution(-1) == 0
+        assert subject.solution(0) == 0
+        assert subject.solution(3) == 0
+        assert subject.solution(4) == 3
+        assert subject.solution(6) == 8
         assert subject.solution(10) == 23
