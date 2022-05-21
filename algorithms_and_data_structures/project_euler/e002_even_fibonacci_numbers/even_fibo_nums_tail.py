77c61189973aacb7fb14d02ed1c3a34c64a479e4
JCPedroza <nemesis4go10@hotmail.com>
2022-05-21 11:52:17 -0500

    vari (eule): implement tail analytic variant for euler 2

---
+++ algorithms_and_data_structures/project_euler/e002_even_fibonacci_numbers/even_fibo_nums_tail.py
@@ -0,0 +1,17 @@
+def fibo_even_sum(limit: int) -> int:
+    """Generate even fibonacci numbers <= the given limit, directly
+    calculating only even members. (no trial division).
+
+    :param limit: Max value of the output.
+    :return: Even fibonacci number.
+    """
+    def loop(acc, fibo, nxt_fibo):
+        if (fibo > limit):
+            return acc
+        return loop(acc + fibo, nxt_fibo, 4 * nxt_fibo + fibo)
+
+    return loop(0, 2, 8)
+
+
+solution = fibo_even_sum
+name = 'tail recursion with analytic optimization'
