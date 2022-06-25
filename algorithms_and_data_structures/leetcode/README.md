df13836a46c10339f27ce68a28eefabe620fcb53
JCPedroza <nemesis4go10@hotmail.com>
2022-06-25 05:34:52 -0500

    feat (leet): test, implement, document problem 1

---
+++ algorithms_and_data_structures/leetcode/README.md
@@ -0,0 +1,7 @@
+# LeetCode
+
+Solutions to LeetCode-like problems, with more generalized requirements.
+
+## Resources
+
+- [LeetCode](https://www.leetcode.com)
---
+++ algorithms_and_data_structures/leetcode/leet0001_two_sum/two_sum_naive.py
@@ -0,0 +1,23 @@
+def two_sum(nums: list[int], target: int) -> tuple[int, int]:
+    """ Finds the index of two integers in a list where their sum equals the given
+    target value.
+
+    Time complexity: O(n²)
+
+    :param nums: List of integers.
+    :param target: Value that the pair of integers have to ad up to.
+    :raises ValueError: Raise error if no solution is found.
+    :returns: A tuple with the index of the pair of integers that add up to target.
+    """
+
+    for pivot_id, pivot in enumerate(nums):
+        for addend_id, addend in enumerate(nums[pivot_id + 1:], pivot_id + 1):
+            if pivot + addend == target:
+                return pivot_id, addend_id
+
+    # If we assume that there's always a solution, this should never be reached
+    raise ValueError("Sum not found")
+
+
+solution = two_sum
+name = "naive"
