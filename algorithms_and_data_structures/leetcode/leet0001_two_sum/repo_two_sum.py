594478fa96577e2c08d0f01fb5bea8d4fc80557a
JCPedroza <nemesis4go10@hotmail.com>
2022-08-31 15:39:50 -0500

    refa: use solution repository

---
+++ algorithms_and_data_structures/leetcode/leet0001_two_sum/repo_two_sum.py
@@ -0,0 +1,5 @@
+from . import two_sum_naive
+from . import two_sum_buff
+
+
+solutions = [two_sum_naive, two_sum_buff]
--- algorithms_and_data_structures/leetcode/leet0001_two_sum/test_two_sum.py
+++ algorithms_and_data_structures/leetcode/leet0001_two_sum/test_two_sum.py
@@ -1,11 +1,10 @@
-from . import two_sum_naive
-from . import two_sum_buff
+from . import repo_two_sum
 
-test_subjects = [two_sum_naive, two_sum_buff]
+solutions = repo_two_sum.solutions
 
 
 def test_two_sum():
-    for subject in test_subjects:
+    for subject in solutions:
         assert subject.solution([8, 3], 11) == (0, 1)
         assert subject.solution([1, 5, 3, 2], 7) == (1, 3)
         assert subject.solution([-3, 4, 7, 9, 1, 8, 2, -2], 15) == (2, 5)
