6820ca2aa14259cc1c61b08dd79f310c2ed2146b
JCPedroza <nemesis4go10@hotmail.com>
2024-04-20 21:38:48 -0600

    refa: add docstrings

--- algorithms_and_data_structures/algorithms/sorting/bubble_sort/bubble_sort_naive.py
+++ algorithms_and_data_structures/algorithms/sorting/bubble_sort/bubble_sort_naive.py
@@ -1,8 +1,20 @@
+"""
+This implementation of Bubble Sort:
+  * Is a naive version, can be easily optimized.
+  * Doesn't return early if the list is already sorted or is sorted during the process.
+  * Iterates through the sorted portion of the list unnecessarily.
+
+Time Complexity: Best O(n^2) | Avg O(n^2) | Worst O(n^2)
+Space Complexity: Total O(n) | Aux O(1)
+"""
+
+
 def bubble_sort(nums: list[float]) -> list[float]:
+    """Sorts a list of floats, in-place."""
     for _ in range(len(nums)):
-        for indx in range(len(nums) - 1):
-            if nums[indx] > nums[indx + 1]:
-                nums[indx], nums[indx + 1] = nums[indx + 1], nums[indx]
+        for idx in range(len(nums) - 1):
+            if nums[idx] > nums[idx + 1]:
+                nums[idx], nums[idx + 1] = nums[idx + 1], nums[idx]
 
     return nums
 
--- algorithms_and_data_structures/algorithms/sorting/bubble_sort/bubble_sort_optimized.py
+++ algorithms_and_data_structures/algorithms/sorting/bubble_sort/bubble_sort_optimized.py
@@ -1,13 +1,25 @@
+"""
+This implementation of Bubble Sort:
+  * Is an optimized version of the naive version.
+  * Returns early if the list is already sorted or is sorted during the process.
+  * Doesn't iterate through the sorted portion of the list unnecessarily.
+
+Time Complexity: Best O(n) | Avg O(n^2) | Worst O(n^2)
+Space Complexity: Total O(n) | Aux O(1)
+"""
+
+
 def bubble_sort(nums: list[float]) -> list[float]:
+    """Sorts a list of floats, in-place."""
     is_sorted = True
 
     for loop in range(len(nums) - 1):
-        for indx in range(len(nums) - loop - 1):
-            if nums[indx] > nums[indx + 1]:
+        for idx in range(len(nums) - loop - 1):
+            if nums[idx] > nums[idx + 1]:
                 is_sorted = False
-                nums[indx], nums[indx + 1] = nums[indx + 1], nums[indx]
+                nums[idx], nums[idx + 1] = nums[idx + 1], nums[idx]
 
-        if is_sorted:
+        if is_sorted:  # End early, list is sorted
             break
 
     return nums
